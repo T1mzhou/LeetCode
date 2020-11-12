4bac70747dcd2ac32c291cd8d30f3e153caf6081
T1mzhou <zhoutianwenzzza@outlook.com>
2020-11-12 22:26:41 +0800

    148-149

---
+++ code/148.sortList.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+
+        ListNode *fast = head, *slow = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        fast = slow;
+        slow = slow->next;;
+        fast->next = nullptr;
+
+        ListNode *l1 = sortList(head);
+        ListNode *l2 = sortList(slow);
+        return mergeTwoLists(l1, l2);
+    }
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode dummy(-1);
+        for (ListNode* p = &dummy; l1 != nullptr || l2 != nullptr; p = p->next) {
+            int val1 = l1 == nullptr ? INT_MAX : l1->val;
+            int val2 = l2 == nullptr ? INT_MAX : l2->val;
+            if (val1 <= val2) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            }   
+        }
+        return dummy.next;
+    }
+};
