68e8b69811973daf15a239c99368c1768e075295
T1mzhou <zhoutianwenzzza@outlook.com>
2020-11-19 21:36:25 +0800

    168 169

---
+++ code/169.majorityElement.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int r, c = 0;
+        for (auto x : nums) {
+            if (!c) r = x, c = 1;
+            else if (r == x) c++;
+            else c--;
+        }
+        return r;
+    }
+};
