f8713bcb973190180b1d9a60668940825d1f3dea
T1mzhou <zhoutianwenzzza@outlook.com>
2021-01-28 19:43:19 +0800

    236

---
+++ code/236.lowestCommonAncestor.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* ans = NULL;
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        dfs(root, p, q);
+        return ans;
+    }
+
+    int dfs(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) return 0;
+        int state = dfs(root->left, p, q);
+        if (root == p) state |= 1;
+        else if (root == q) state |= 2;
+        state |= dfs(root->right, p, q);
+        if (state == 3 && !ans) ans = root;
+        return state; 
+    }
+};
